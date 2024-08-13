2d03f1e98a819cb53917a3b642be31d119593a69
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-08-13 12:13:08 -0500

    Increment version

--- src/main/java/dev/rndmorris/hqmkeybind/HqmKeybind.java
+++ src/main/java/dev/rndmorris/hqmkeybind/HqmKeybind.java
@@ -22,7 +22,7 @@ public class HqmKeybind {
     public static final String DEPS = "after:HardcoreQuesting";
     public static final String MODID = "hqmkeybind";
     public static final String NAME = "HQM Keybind";
-    public static final String VERSION = "1.7.10-A1";
+    public static final String VERSION = "1.7.10-A2";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
     @SidedProxy(
